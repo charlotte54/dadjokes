27f940af9dbd374ec81c92e688174cabfdef2c72
charlotte8592 <charlotte8592@gmail.com>
2022-11-24 16:32:33 +0000

    Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, LinkContainer } from "react-router-bootstrap"
+import { LinkContainer } from "react-router-bootstrap"
 import { Navbar, Nav, Container } from "react-bootstrap"
 
 const Header = () => {
